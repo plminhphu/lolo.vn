34eb85d035d5cf2e3936c472ccc584753a0cb7f3
Phạm Lê Minh Phú <34944968+phamleminhphu@users.noreply.github.com>
2019-05-21 20:54:57 +0700

    remove all

--- lolo_login_app_customer/lib/UI/util/string.dart
+++
@@ -1,7 +0,0 @@
-String stringNull = "";
-String stringTitleSignIn = "Sign In";
-String stringTitlePhoneVerifi = "Phone Verification";
-String stringInputPhone = "Login with your phone number";
-String stringSpanOTPInput = "Enter your OTP code here";
-String stringNext = "Next";
-String stringPhoneERR = "Not fond your phone number";
